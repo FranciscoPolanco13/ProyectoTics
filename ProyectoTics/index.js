3d35b5c4e1013aa577f2333d0e76fc71cbe6f7e9
FranciscoPolanco13 <francisco.polanco@mail.udp.cl>
2025-05-28 15:40:41 -0400

    Update index.js

--- ProyectoTics/index.js
+++ ProyectoTics/index.js
@@ -108,4 +108,6 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 
